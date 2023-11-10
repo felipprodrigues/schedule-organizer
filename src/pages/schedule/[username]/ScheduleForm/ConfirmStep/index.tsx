21f6c1a7ae38e45298e4ce6e12f6df45e8858765
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-11-10 15:57:46 -0300

    feat: add confirmStep page and zod schema

---
+++ src/pages/schedule/[username]/ScheduleForm/ConfirmStep/index.tsx
@@ -0,0 +1,68 @@
+import { Button, Text, TextInput } from '@ignite-ui/react';
+import { ConfirmForm, FormActions, FormError, FormHeader } from './styles';
+import { CalendarBlank, Clock } from 'phosphor-react';
+import { z } from 'zod';
+import { useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+
+const confirmFormSchema = z.object({
+  name: z.string().min(3, { message: 'O nome precisa no mínimo 3 caracteres' }),
+  email: z.string().email({ message: 'Digite um email válido' }),
+  observations: z.string().nullable(),
+});
+
+type ConfirmFormData = z.infer<typeof confirmFormSchema>;
+
+export function ConfirmStep() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm<ConfirmFormData>({
+    resolver: zodResolver(confirmFormSchema),
+  });
+
+  function handleConfirmScheduling() {}
+
+  return (
+    <ConfirmForm as="form" onSubmit={handleSubmit(handleConfirmScheduling)}>
+      <FormHeader>
+        <Text>
+          <CalendarBlank />
+          22 de setembro de 2023
+        </Text>
+
+        <Text>
+          <Clock />
+          18:00h
+        </Text>
+      </FormHeader>
+
+      <label>
+        <Text size="sm">Nome completo</Text>
+        <TextInput placeholder="Seu nome" {...register('name')} />
+        {errors.name && <FormError size="sm">{errors.name.message}</FormError>}
+      </label>
+      <label>
+        <Text size="sm">Endereço de email</Text>
+        <TextInput placeholder="Seu nome" type="email" {...register('email')} />
+        {errors.email && (
+          <FormError size="sm">{errors.email.message}</FormError>
+        )}
+      </label>
+      <label>
+        <Text size="sm">Observações</Text>
+        <TextInput {...register('observations')} />
+      </label>
+
+      <FormActions>
+        <Button type="button" variant="tertiary">
+          Cancelar
+        </Button>
+        <Button type="submit" disabled={isSubmitting}>
+          Confirmar
+        </Button>
+      </FormActions>
+    </ConfirmForm>
+  );
+}
