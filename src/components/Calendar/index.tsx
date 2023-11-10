f9edcd5f192874854fdb27f603b443dd7d9ac71a
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-11-10 19:22:16 -0300

    feat: add dayjs lib to calculate months

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -8,23 +8,43 @@ import {
   CalendarTitle,
 } from './styles';
 import { getWeekDays } from '@/utils/getWeekDays';
+import dayjs from 'dayjs';
+import { useState } from 'react';
 
 export function Calendar() {
+  const [currentDate, setCurrentDate] = useState(() => {
+    return dayjs().set('date', 1);
+  });
+
+  function handlePreviousMonth() {
+    const previousMonthDate = currentDate.subtract(1, 'month');
+
+    setCurrentDate(previousMonthDate);
+  }
+
+  function handleNextMonth() {
+    const previousNextDate = currentDate.add(1, 'month');
+
+    setCurrentDate(previousNextDate);
+  }
+
   const shortWeekDays = getWeekDays({ short: true });
+
+  const currentMonth = currentDate.format('MMMM');
+  const currentYear = currentDate.format('YYYY');
 
   return (
     <CalendarContainer>
       <CalendarHeader>
         <CalendarTitle>
-          Novembro
-          <span>2023</span>
+          {currentMonth} <span>{currentYear}</span>
         </CalendarTitle>
 
         <CalendarActions>
-          <button>
+          <button onClick={handlePreviousMonth} title="Previous month">
             <CaretLeft />
           </button>
-          <button>
+          <button onClick={handleNextMonth} title="Next month">
             <CaretRight />
           </button>
         </CalendarActions>
