55dd731718dcee5ce2f394e7591ce85668b5262e
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-11-16 16:45:45 -0300

    feat: add scheduling to db and user availability api

--- src/components/Calendar/index.tsx
+++ src/components/Calendar/index.tsx
@@ -10,7 +10,6 @@ import {
 import { getWeekDays } from '@/utils/getWeekDays';
 import dayjs from 'dayjs';
 import { useMemo, useState } from 'react';
-import { date } from 'zod';
 
 interface CalendarWeek {
   week: number;
@@ -22,7 +21,12 @@ interface CalendarWeek {
 
 type CalendarWeeks = CalendarWeek[];
 
-export function Calendar() {
+interface CalendarProps {
+  selectedDate: Date | null;
+  onDateSelected: (date: Date) => void;
+}
+
+export function Calendar({ selectedDate, onDateSelected }: CalendarProps) {
   const [currentDate, setCurrentDate] = useState(() => {
     return dayjs().set('date', 1);
   });
@@ -78,7 +82,7 @@ export function Calendar() {
         return { date, disabled: true };
       }),
       ...daysInMonthArray.map((date) => {
-        return { date, disabled: false };
+        return { date, disabled: date.endOf('day').isBefore(new Date()) };
       }),
       ...nextMonthFillArray.map((date) => {
         return { date, disabled: true };
@@ -137,7 +141,10 @@ export function Calendar() {
                 {days.map(({ date, disabled }) => {
                   return (
                     <td key={date.toString()}>
-                      <CalendarDay disabled={disabled}>
+                      <CalendarDay
+                        disabled={disabled}
+                        onClick={() => onDateSelected(date.toDate())}
+                      >
                         {date.get('date')}
                       </CalendarDay>
                     </td>
