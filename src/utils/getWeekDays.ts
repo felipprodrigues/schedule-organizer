3f25018b026a98428ea8ec628d415e4150da03e9
felipprodrigues <dev.rodriguesfelipp@gmail.com>
2023-11-28 13:10:08 -0300

    fiz: remove param from keys method in getweekdays

--- src/utils/getWeekDays.ts
+++ src/utils/getWeekDays.ts
@@ -5,7 +5,7 @@ interface GetWeekDaysParams {
 export function getWeekDays({ short = false }: GetWeekDaysParams = {}) {
   const formatter = new Intl.DateTimeFormat('pt-BR', { weekday: 'long' })
 
-  return Array.from(Array(7).keys(0))
+  return Array.from(Array(7).keys())
     .map((day) => formatter.format(new Date(Date.UTC(2021, 5, day))))
     .map((weekday) => {
       if (short) {
